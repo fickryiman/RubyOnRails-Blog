03c4d3696b73c8048fefe0a5f6e512521221c337
Fickry Iman <fickry.iman@gmail.com>
2023-11-06 13:41:08 +0700

    add gem bullet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :desc)
+    # @posts = Post.all
   end
 
   private
