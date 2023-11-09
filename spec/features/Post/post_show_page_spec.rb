6a7a571ccd0b592a6d24bbe6e43eee47f311dfc1
Fickry Iman <fickry.iman@gmail.com>
2023-11-09 23:50:48 +0700

    fixes linters error #2

--- spec/features/Post/post_show_page_spec.rb
+++ spec/features/Post/post_show_page_spec.rb
@@ -11,34 +11,29 @@ RSpec.feature 'Post Show Page' do
 
   scenario 'Displays post title and author' do
     visit user_post_path(user1, post1)
-
     expect(page).to have_content(post1.title)
     expect(page).to have_content(user1.name)
   end
 
   scenario 'Displays how many comments and likes the post has' do
     visit user_post_path(user1, post1)
-
     expect(page).to have_content("Comments: #{post1.comments.count}")
     expect(page).to have_content("Likes: #{post1.likes.count}")
   end
 
   scenario 'Displays the post body' do
     visit user_post_path(user1, post1)
-
     expect(page).to have_content('This is the body of the post.')
   end
 
   scenario 'Displays the username of each commentor' do
     visit user_post_path(user1, post1)
-
     expect(page).to have_content(comment1.user.name)
     expect(page).to have_content(comment2.user.name)
   end
 
   scenario 'Displays the comment left by each commentor' do
     visit user_post_path(user1, post1)
-
     expect(page).to have_content(comment1.text)
     expect(page).to have_content(comment2.text)
   end
--- spec/features/Post/user_post_index_spec.rb
+++ spec/features/Post/user_post_index_spec.rb
@@ -11,24 +11,20 @@ RSpec.feature 'User Post Index Page' do
 
   scenario 'Displays username, profile picture, number of posts and total likes' do
     visit user_posts_path(user)
-
     expect(page).to have_css("img[src='#{user.photo}']")
     expect(page).to have_content(user.name)
-
     expect(page).to have_content("Number of post: #{user.posts.count}")
     expect(page).to have_content("Likes: #{post.likes_counter}")
   end
 
   scenario 'Displays post title and some of the post body' do
     visit user_posts_path(user)
-
     expect(page).to have_content(post.title)
     expect(page).to have_content('This is the body of the post.')
   end
 
   scenario 'Displays the first comments on a post & total comments' do
     visit user_posts_path(user)
-
     expect(page).to have_content(comment2.text)
     expect(page).to have_content("Comments: #{post.comments_counter}")
   end
